837857423788542acc6b00744d486bb81395b123
rico.putra <rico.putra@outlook.co.id>
2020-09-10 14:00:13 +0700

    CREATE home section

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-main>
+      <h1>Home</h1>
+  </v-main>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Home from './components/Home'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home }
+  ],
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
