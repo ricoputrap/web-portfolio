874eca8bacf0a39fae9ad7c1db7c667163789129
rico.putra <rico.putra@outlook.co.id>
2020-09-14 12:57:20 +0700

    Prepare state for projects

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,8 +1,54 @@
-const state = {}
+const state = {
+    projects: [
+        {
+            title: "Project Title",
+            posted: "August 2020",
+            desc: "Deskripsi lorem ipsum dolor si amet amit lah yaaa",
+            stack: [
+                {
+                    name: "python",
+                    color: "#366F9E",
+                }
+            ],
+            medium: "#",
+            github: "#",
+        },
+        {
+            title: "Project Title",
+            posted: "August 2020",
+            desc: "Deskripsi lorem ipsum dolor si amet amit lah yaaa",
+            stack: [
+                {
+                    name: "python",
+                    color: "#366F9E",
+                }
+            ],
+            medium: "#",
+            github: "#",
+        },
+        {
+            title: "Project Title",
+            posted: "August 2020",
+            desc: "Deskripsi lorem ipsum dolor si amet amit lah yaaa",
+            stack: [
+                {
+                    name: "python",
+                    color: "#366F9E",
+                }
+            ],
+            medium: "#",
+            github: "#",
+        }
+    ],
+}
 
-const getters = {};
+const getters = {
+    allProjects: (state) => state.projects,
+}
 
-const mutations = {}
+const mutations = {
+    // setProjects: (state, projects) => (state.projects = projects),
+}
 
 const actions = {}
 
